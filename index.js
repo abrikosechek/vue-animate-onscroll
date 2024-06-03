07dcf73f02eda679122075af31da777bb8c2f185
abrikosechek <abrikosechek@gmail.com>
2024-06-03 19:35:27 +0400

    directive

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function helloNpm() {
-  return "hello NPM";
-}
+const animatedScrollObserver = (className) =>
+  new IntersectionObserver((entries, animatedScrollObserver) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add(`enter-${className}`);
+        animatedScrollObserver.unobserve(entry.target);
+      }
+    });
+  });
 
-module.exports = helloNpm;
+export default {
+  created(el, binding) {
+    const classModifier = binding.value || "onscroll";
+    el.classList.add(`before-enter-${classModifier}`);
+    animatedScrollObserver(classModifier).observe(el);
+  },
+};
